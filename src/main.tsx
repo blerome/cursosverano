6327941af99b1db1dc71dde0cff9d0ef07a7fa9f
blerome <105025286+blerome@users.noreply.github.com>
2025-05-18 21:04:14 -0500

    Update main.tsx para que ya funcione el boton de login :C

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import ReactDOM from 'react-dom/client'
 import React from 'react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { MsalProvider } from '@azure/msal-react';
+import { msalInstance } from './auth/msalConfig'; 
 import App from './App'
 
 const queryClient = new QueryClient();
@@ -8,7 +10,9 @@ const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
+        <MsalProvider instance={msalInstance}>
       <App />
+      </MsalProvider>
     </QueryClientProvider>
   </React.StrictMode>
 );
